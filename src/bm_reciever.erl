9553305e10b886363c567d756502ad25030b0896
t0ha <t0hashvein@gmail.com>
2013-08-20 15:55:57 +0600

    Version exchenge pre

--- src/bm_reciever.erl
+++ src/bm_reciever.erl
@@ -2,7 +2,7 @@
 -compile([export_all]).
 -include("../include/bm.hrl").
 
--record(state, {socket, transport, version, stream = 1, init_stage = 0, remote_streams}).
+-record(state, {socket, transport, version, stream = 1, init_stage = 0, remote_streams, remote_addr}).
 -record(init_stage, {verack_sent=0,verack_recv=0}).
 
 loop(#state{socket = Socket, transport = Transport}=IState) ->
@@ -24,28 +24,29 @@ loop(#state{socket = Socket, transport = Transport}=IState) ->
 
 analyse_packet(<<"version", _>>, Length, <<Version:32/integer,
                                            1:64/integer, % 'services' field of protocol
-                                           Time:64/integer,
-                                           AddrRecv:26/bytes,
+                                           _Time:64/integer,
+                                           _AddrRecv:26/bytes,
                                            AddrFrom:26/bytes,
-                                           Nonce:8/bytes,
+                                           _Nonce:8/bytes,
                                            Data/bytes>>,
               State) 
         when Length > 83, Version > 1 ->
-    {UA, StremsL} = bm_types:decode_varstr(Data),
+    {_UA, StremsL} = bm_types:decode_varstr(Data),
     {Streams, _} = bm_types:decode_intlist(StremsL),
     #state{transport=Transport, socket=Socket} = State,
     % Sending verack
     Transport:send(Socket, <<?MAGIC/bytes, "verack", 0:6/unit:8-integer, 0:32/integer, 16#cf83e135:32/big-integer>>),
     Stage = State#state.init_stage#init_stage{verack_sent=true},
+
     if Stage#init_stage.verack_recv == false ->
-            send_version(Stage)
+            send_version(State)
     end,
-    State#state{version=Version, init_stage=Stage, remote_streams=Streams};
+    State#state{version=Version, init_stage=Stage, remote_streams=Streams, remote_addr=bm_types:decode_network(AddrFrom)};
 analyse_packet(<<"verack", _>>, 0, <<>>, State) ->
     State#state{init_stage=State#state.init_stage#init_stage{verack_recv=true}};
 analyse_packet(<<"addr", _>>, Length, Data, #state{init_stage=#init_stage{verack_recv=true, verack_sent=true}} = State) ->
     {Len, AList} = bm_types:decode_varint(Data),
-    Addrs = decode_addr_list(AList, Len, Acc),
+    Addrs = decode_addr_list(AList, Len),
     dets:insert(addr, Addrs),
     State;
 analyse_packet(<<"getpubkey", _>>, Length, <<PNonce:64/big-integer,
@@ -73,22 +74,32 @@ analyse_packet(<<"broadcast", _>>, Length, Packet,
                  #state{init_stage=#init_stage{verack_recv=true, verack_sent=true}} = State) ->
     impl;
 analyse_packet(<<"ping", _>>, Length, Packet,
-                 #state{init_stage=#init_stage{verack_recv=true, verack_sent=true}} = State) ->
+                 #state{transport=Transport, socket=Socket,init_stage=#init_stage{verack_recv=true, verack_sent=true}} = State) ->
     % Sending pong
     Transport:send(Socket, <<?MAGIC/bytes, "pong", 0:8/unit:8-integer, 0:32/integer, 16#cf83e135:32/big-integer>>);
 analyse_packet(_, _, _,
                  #state{init_stage=#init_stage{verack_recv=true, verack_sent=true}} = State) ->
     State.
                
-send_version() ->
+send_version(#state{transport=Transport, socket=Socket, stream=Stream, remote_addr=RAddr } = State) ->
     {MSec, Sec, _} = now(),
     Time = MSec * 1.0e6 + Sec,
-    AddrRecv = encode_address(#network_address{time=Time, stream=State#state.stream, ip=Ip, port=Port}),
-    AddrFrom = encode_address(#network_address{time=Time, stream=State#state.stream, ip=Ip, port=Port}),
+    {ok, {Ip, Port}} = inet:sockname(Socket),
+    <<_:12/bytes, AddrRecv/bytes>> = bm_types:encode_address(RAddr),
+    <<_:12/bytes, AddrFrom/bytes>> = bm_types:encode_address(#network_address{time=Time, stream=Stream, ip=Ip, port=Port}),
+    Nonce = crypto:rand_bytes(8),
     Message = <<2:32/integer,
                 1:64/integer, % 'services' field of protocol
                 Time:64/integer,
                 AddrRecv:26/bytes,
                 AddrFrom:26/bytes,
                 Nonce:8/bytes,
-                Data/bytes>>,
+                (bm_types:encode_varstr("/BitMessageErl:0.1/"))/bytes,
+                (bm_types:encode_intlist([1, Stream]))/bytes>>,
+    Transport:send(Socket, Message).
+
+decode_addr_list(_, 0) ->
+    [];
+decode_addr_list(<<Addr:34/bytes, Rest/bytes>>, Length) ->
+    [bm_types:decode_network(Addr) | decode_addr_list(Rest, Length - 1)].
+    
