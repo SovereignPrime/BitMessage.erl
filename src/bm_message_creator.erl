6459f1cf70cd3aa40094e29e9fde881791253c49
Shvein Anton <t0hashvein@gmail.com>
2014-10-16 13:48:39 +0600

    Style fixes

--- src/bm_message_creator.erl
+++ src/bm_message_creator.erl
@@ -3,14 +3,14 @@
 
 -include("../include/bm.hrl").
 
-create_message(Command, Payload) ->
+create_message(Command, Payload) ->  % {{{1
     Length = size(Payload),
     CL = byte_size(Command),
     C = <<Command/bytes, 0:(12 - CL)/unit:8>>,
     <<Check:4/bytes, _/bytes>> = crypto:hash(sha512, Payload),
     <<?MAGIC, C:12/bytes, Length:32/big-integer, Check:4/bytes, Payload/bytes>>.
 
-create_obj(Hash) ->
+create_obj(Hash) ->  % {{{1
     case bm_db:lookup(inventory, Hash) of
             [#inventory{type=Type, payload=Payload}] -> 
             create_message(Type, Payload);
@@ -18,10 +18,10 @@ create_obj(Hash) ->
             error_logger:warning_msg("Can't find inv ~p~n", [Hash])
     end.
 
-create_inv(Hash) ->
+create_inv(Hash) ->  % {{{1
     create_message(<<"inv">>, bm_types:encode_list(Hash, fun(H) -> H end)).
 
-create_big_inv(Stream, Exclude) ->
+create_big_inv(Stream, Exclude) ->  % {{{1
     {ok, PubKeyAge} = application:get_env(bitmessage, 'max_age_of_public_key'),
     {ok, InvAge} = application:get_env(bitmessage, 'max_age_of_inventory'),
     {MSec, Sec, _} = now(),
@@ -36,7 +36,7 @@ create_big_inv(Stream, Exclude) ->
                 create_message(<<"inv">>, bm_types:encode_list(Inv, fun(O) -> <<O/bytes>> end))
         end, InvList).
 
-create_addrs_for_stream(Stream) ->
+create_addrs_for_stream(Stream) ->  % {{{1
     {ok, NodeAge} = application:get_env(bitmessage, 'max_age_of_node'),
     {MSec, Sec, _} = now(),
     Time = trunc(MSec * 1.0e6 + Sec),
@@ -50,7 +50,11 @@ create_addrs_for_stream(Stream) ->
                     create_message(<<"addr">>, bm_types:encode_list(Hash, fun bm_types:encode_network/1))
         end, Hashes).
 
-create_pubkey(#privkey{hash=RIPE, psk=PSK, pek=PEK, public=Pub, address=Addr}) ->
+create_pubkey(#privkey{hash=RIPE,  % {{{1
+                       psk=PSK,
+                       pek=PEK,
+                       public=Pub,
+                       address=Addr}) ->
     Time = bm_types:timestamp() + crypto:rand_uniform(-300, 300),
     #address{stream=Stream, version=AVer} = bm_auth:decode_address(Addr),
     Payload = <<Time:64/big-integer,
@@ -72,7 +76,7 @@ create_pubkey(#privkey{hash=RIPE, psk=PSK, pek=PEK, public=Pub, address=Addr}) -
                                         stream=Stream}]),
     create_inv([ Hash ]).
                                        
-create_getpubkey(#address{ripe=RIPE, version=Version, stream=Stream}) ->
+create_getpubkey(#address{ripe=RIPE, version=Version, stream=Stream}) ->  % {{{1
     Time = bm_types:timestamp() + crypto:rand_uniform(-300, 300),
     UPayload = <<(bm_types:timestamp() + crypto:rand_uniform(-300, 300)):64/big-integer,
                  (bm_types:encode_varint(Version))/bytes,
@@ -88,7 +92,7 @@ create_getpubkey(#address{ripe=RIPE, version=Version, stream=Stream}) ->
                                         stream=Stream}]),
     create_inv([ Hash ]).
 
-create_ack(#message{ackdata=Payload, from=Addr}) ->
+create_ack(#message{ackdata=Payload, from=Addr}) ->  % {{{1
     <<_:16/bytes, PLen:32/big-integer, _:4/bytes, PPayload/bytes>> = Payload,
     <<Hash:32/bytes, _/bytes>> = bm_auth:dual_sha(PPayload),
     error_logger:info_msg("Sending ack: ~p~n", [bm_types:binary_to_hexstring(Hash)]),
