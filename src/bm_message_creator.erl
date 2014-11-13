0974bfd73c2556d4a0ad3597bc82f0e546dde577
Shvein Anton <t0hashvein@gmail.com>
2014-11-13 14:24:07 +0500

    Some fixes to bm_message_creator

--- src/bm_message_creator.erl
+++ src/bm_message_creator.erl
@@ -6,10 +6,10 @@
 %% @doc Creates message 
 %%
 %% Creates message from `Command` and `Payload`
--spec create_message(Command, Payload) -> message_bin() when
+-spec create_message(Command, Payload) -> message_bin() when  % {{{1
       Command :: binary(),
       Payload :: binary().
-create_message(Command, Payload) ->  % {{{1
+create_message(Command, Payload) ->
     Length = size(Payload),
     CL = byte_size(Command),
     C = <<Command/bytes, 0:(12 - CL)/unit:8>>,
@@ -25,9 +25,9 @@ create_message(Command, Payload) ->  % {{{1
 %%
 %% Creates object message looking inventory for `Hash`
 %% in database and creating `Message`
--spec create_obj([Hash]) -> message_bin() when 
+-spec create_obj([Hash]) -> message_bin() when   % {{{1
       Hash ::binary().
-create_obj(Hash) ->  % {{{1
+create_obj(Hash) ->
     case bm_db:lookup(inventory, Hash) of
             [#inventory{type=Type, payload=Payload}] -> 
             create_message(Type, Payload);
@@ -38,10 +38,10 @@ create_obj(Hash) ->  % {{{1
 %% @doc Creates inv message from inventory hash list
 %%
 %% Creates inv message from hashlist `Hash`
--spec create_inv(Hashes) -> message_bin() when
+-spec create_inv(Hashes) -> message_bin() when  % {{{1
       Hashes :: [Hash],
       Hash :: binary().
-create_inv(Hashes) ->  % {{{1
+create_inv(Hashes) ->
     create_message(<<"inv">>, bm_types:encode_list(Hashes, fun(H) -> H end)).
 
 %% @doc Creates inv message finding data in db
@@ -49,10 +49,10 @@ create_inv(Hashes) ->  % {{{1
 %% Creates inv message finding data in db by `Stream` 
 %% TODO: make `Excludes` work to exclude already known invs
 %% Takes age parameters from config file
--spec create_big_inv(Stream, Exclude) -> message_bin() when
+-spec create_big_inv(Stream, Exclude) -> message_bin() when  % {{{1
       Stream :: integer(),
       Exclude :: list().
-create_big_inv(Stream, Exclude) ->  % {{{1
+create_big_inv(Stream, Exclude) ->
     {ok, PubKeyAge} = application:get_env(bitmessage, 'max_age_of_public_key'),
     {ok, InvAge} = application:get_env(bitmessage, 'max_age_of_inventory'),
     {MSec, Sec, _} = now(),
@@ -87,9 +87,9 @@ create_big_inv(Stream, Exclude) ->  % {{{1
 %% @doc Creates addr message
 %%
 %% Creates addr message for `Stream`
--spec create_addrs_for_stream(Stream) -> message_bin() when
+-spec create_addrs_for_stream(Stream) -> message_bin() when  % {{{1
       Stream :: integer().
-create_addrs_for_stream(Stream) ->  % {{{1
+create_addrs_for_stream(Stream) ->
     {ok, NodeAge} = application:get_env(bitmessage, 'max_age_of_node'),
     {MSec, Sec, _} = now(),
     Time = trunc(MSec * 1.0e6 + Sec),
@@ -160,7 +160,7 @@ create_getpubkey(#address{ripe=RIPE,
 %% Creates ack message object and saves it to DB and 
 %% returnd inv message
 -spec create_getpubkey(#message{}) -> message_bin().  % {{{1
-create_ack(#message{ackdata=Payload, from=Addr}) ->  % {{{1
+create_ack(#message{ackdata=Payload, from=Addr}) ->
     <<_:16/bytes, PLen:32/big-integer, _:4/bytes, PPayload/bytes>> = Payload,
     <<Hash:32/bytes, _/bytes>> = bm_auth:dual_sha(PPayload),
     error_logger:info_msg("Sending ack: ~p~n", [bm_types:binary_to_hexstring(Hash)]),
