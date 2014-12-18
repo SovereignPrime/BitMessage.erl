f18aea24eefa492d6503b4e4135818ae6de7705f
Shvein Anton <t0hashvein@gmail.com>
2014-12-18 15:06:01 +0500

    Pow and sqrt on long integers and tests for it

--- src/bm_types.erl
+++ src/bm_types.erl
@@ -6,14 +6,14 @@
 -export_type([
               timestamp/0
              ]).
-%% Unix Timestamp type
+%% Unix Timestamp type  % {{{1
 -type timestamp() :: integer().
 
 %%%
-%% Integer packing and unpacking to VariantInt
+%% Integer packing and unpacking to VariantInt  % {{{1
 %%%
 
--spec encode_varint(integer()) -> binary().
+-spec encode_varint(integer()) -> binary().  % {{{2
 encode_varint(Num) when Num < 16#fd ->
     <<Num:8>>;
 encode_varint(Num) when Num =< 16#ffff ->
@@ -23,7 +23,7 @@ encode_varint(Num) when Num =< 16#ffffffff ->
 encode_varint(Num) ->
     <<16#ff,Num:64>>.
 
--spec decode_varint(<<_:8, _:_*1>>) -> {non_neg_integer(), binary()}.
+-spec decode_varint(<<_:8, _:_*1>>) -> {non_neg_integer(), binary()}.  % {{{2
 decode_varint(<<Len:8/big-integer, Rest/bits>>) when Len <16#fd ->
     {Len, Rest};
 decode_varint(<<16#fd, Num:16/big-integer, Rest/bits>>) ->
@@ -34,42 +34,42 @@ decode_varint(<<16#ff, Num:64/big-integer, Rest/bits>>) ->
     {Num, Rest}.
 
 %%%
-%% String packing and unpacking to VariantStr
+%% String packing and unpacking to VariantStr  % {{{1
 %%%
 
--spec encode_varstr(string()) -> binary().
+-spec encode_varstr(string()) -> binary().  % {{{2
 encode_varstr(Str) ->
     Len = length(Str),
     <<(encode_varint(Len))/bytes, (list_to_binary(Str))/bytes>>.
 
--spec decode_varstr(binary()) -> {string(), binary()}.
+-spec decode_varstr(binary()) -> {string(), binary()}.  % {{{2
 decode_varstr(VStr) ->
     {Len, S} = decode_varint(VStr),
     <<Str:Len/bytes, Rest/bytes>> = S,
     {binary_to_list(Str), Rest}.
 
--spec decode_varbin(binary()) -> {binary(), binary()}.
+-spec decode_varbin(binary()) -> {binary(), binary()}.  % {{{2
 decode_varbin(VStr) ->
     {Len, S} = decode_varint(VStr),
     <<Str:Len/bytes, Rest/bytes>> = S,
     {Str, Rest}.
 
 %%%
-%% List of any packing and unpacking to VariantIntList
+%% List of any packing and unpacking to VariantIntList  % {{{1
 %%%
 
--spec encode_list(list(TList), fun((TList) -> binary())) -> binary().
+-spec encode_list(list(TList), fun((TList) -> binary())) -> binary().  % {{{2
 encode_list(Lst, Fun) ->
     Len = length(Lst),
     BLst = << <<(Fun(I))/bytes>> || I <- Lst>>,
     <<(encode_varint(Len))/bytes, BLst/bytes>>.
 
--spec decode_list(binary(), fun((TList) -> binary())) -> {list(TList), binary()}.
+-spec decode_list(binary(), fun((TList) -> binary())) -> {list(TList), binary()}.  % {{{2
 decode_list(VLst, Fun) ->
     {Len, S} = decode_varint(VLst),
     decode_list(S, Len, [], Fun).
 
--spec decode_list(binary(), non_neg_integer(), list(), fun((TList) -> binary())) -> {list(TList), binary()}.
+-spec decode_list(binary(), non_neg_integer(), list(), fun((TList) -> binary())) -> {list(TList), binary()}.  % {{{2
 decode_list(B, 0, A, _Fun) ->
     {A, B};
 decode_list(B, C, A, Fun) ->
@@ -77,10 +77,10 @@ decode_list(B, C, A, Fun) ->
      decode_list(R, C - 1, A ++ [I], Fun).
 
 %%%
-%% Network address packing and unpacking to NetworkAddressStruct
+%% Network address packing and unpacking to NetworkAddressStruct  % {{{1
 %%%
 
--spec encode_network(#network_address{}) -> binary().
+-spec encode_network(#network_address{}) -> binary().  % {{{2
 encode_network(#network_address{time=Time,
                                 stream=Stream,
                                 ip={Ip1,Ip2,Ip3,Ip4},
@@ -95,7 +95,7 @@ encode_network(#network_address{time=Time,
       Ip1, Ip2, Ip3, Ip4,
       Port:16/big-integer>>.
 
--spec decode_network(binary()) -> {#network_address{} | [], binary()}.
+-spec decode_network(binary()) -> {#network_address{} | [], binary()}.  % {{{2
 decode_network(<<Time:64/big-integer,
                  Stream:32/big-integer,
                  1:64/big-integer,
@@ -123,27 +123,42 @@ decode_network(<<Time:64/big-integer,
      R}.
 
 %%%
-%% Helpers
+%% Helpers  % {{{1
 %%%
 
--spec integer_to_bytes(integer()) -> binary().
+-spec integer_to_bytes(integer()) -> binary().  % {{{2
 integer_to_bytes(0) ->
     <<>>;
 integer_to_bytes(Num) ->
     <<(integer_to_bytes(Num div 256))/bits, (Num rem 256)/big-integer>>.
 
--spec binary_to_hexstring(binary()) -> string().
+-spec binary_to_hexstring(binary()) -> string().  % {{{2
 binary_to_hexstring(Data) ->
     lists:flatten([io_lib:format("~2.16.0b", [B]) || <<B>> <= Data]).
 
--spec timestamp() -> integer().
+-spec timestamp() -> integer().  % {{{2
 timestamp() ->
     {MSec, Sec, _} = now(),
     trunc(MSec * 1.0e6 + Sec).
 
--spec pow(integer(), integer()) -> integer().
+%%%
+%% Long Integer Math  % {{{1
+%%%
+-spec pow(integer(), integer()) -> integer().  % {{{2
 pow(_, 0) ->
     1;
 pow(Num, Pow) when Pow >= 0 ->
     Num * pow(Num, Pow - 1).
+
+-spec sqrt(integer()) -> integer().  % {{{2
+sqrt(Num) ->
+    sqrti(Num, 1).
+
+-spec sqrti(integer(), integer()) -> integer().  % {{{2
+sqrti(0, C) ->
+    (C - 1) div 2;
+sqrti(Num, C) when C > 0 ->
+    sqrti(Num - C, C + 2).
+    
+    
 
--- test/bm_types_SUITE.erl
+++ test/bm_types_SUITE.erl
@@ -31,7 +31,11 @@
          encode_varint/0,
          encode_varint/1,
          encode_varstr/0,
-         encode_varstr/1
+         encode_varstr/1,
+         pow/0,
+         pow/1,
+         sqrt/0,
+         sqrt/1
         ]).
 
 -include_lib("common_test/include/ct.hrl").
@@ -40,10 +44,10 @@
 
 
 %%%===================================================================
-%%% Common Test callbacks
+%%% Common Test callbacks  {{{1
 %%%===================================================================
 
-all() ->  % {{{1
+all() ->  % {{{2
     [
      integer_to_bytes,
      decode_encode_list,
@@ -53,94 +57,110 @@ all() ->  % {{{1
      encode_list,
      encode_network,
      encode_varint,
-     encode_varstr
+     encode_varstr,
+     pow,
+     sqrt
     ].
 
-suite() ->  % {{{1
+suite() ->  % {{{2
     [{timetrap, {seconds, 30}}].
 
-groups() ->  % {{{1
+groups() ->  % {{{2
     [].
 
-init_per_suite(Config) ->  % {{{1
+init_per_suite(Config) ->  % {{{2
     Config.
 
-end_per_suite(_Config) ->  % {{{1
+end_per_suite(_Config) ->  % {{{2
     ok.
 
-group(_GroupName) ->  % {{{1
+group(_GroupName) ->  % {{{2
     [].
 
-init_per_group(_GroupName, Config) ->  % {{{1
+init_per_group(_GroupName, Config) ->  % {{{2
     Config.
 
-end_per_group(_GroupName, _Config) ->  % {{{1
+end_per_group(_GroupName, _Config) ->  % {{{2
     ok.
 
-init_per_testcase(_TestCase, Config) ->  % {{{1
+init_per_testcase(_TestCase, Config) ->  % {{{2
     Config.
 
-end_per_testcase(_TestCase, _Config) ->  % {{{1
+end_per_testcase(_TestCase, _Config) ->  % {{{2
     ok.
 
 %%%===================================================================
-%%% Test cases
+%%% Test cases {{{1
 %%%===================================================================
 
 
-integer_to_bytes() ->  % {{{1
+integer_to_bytes() ->  % {{{2
     [].
 
-integer_to_bytes(_Config) ->  % {{{1
+integer_to_bytes(_Config) ->  % {{{2
         ?_assert(integer_to_bytes(crypto:bytes_to_integer(<<"TEST">>)) == <<"TEST">>).
 
-decode_encode_list() ->  % {{{1
+decode_encode_list() ->  % {{{2
     [].
 
-decode_encode_list(_Config) ->  % {{{1
-    eunit:test({generator, fun decode_encode_list_test_/0}).
+decode_encode_list(_Config) ->  % {{{2
+    ok=eunit:test({generator, fun decode_encode_list_test_/0}).
 
-decode_encode_network() ->  % {{{1
+decode_encode_network() ->  % {{{2
     [].
-decode_encode_network(_Config) ->  % {{{1
-    eunit:test({generator, fun decode_encode_network_test_/0}).
+decode_encode_network(_Config) ->  % {{{2
+    ok=eunit:test({generator, fun decode_encode_network_test_/0}).
 
-decode_encode_varint() ->  % {{{1
+decode_encode_varint() ->  % {{{2
     [].
-decode_encode_varint(_Config) ->  % {{{1
-    eunit:test({generator, fun decode_encode_varint_test_/0}).
+decode_encode_varint(_Config) ->  % {{{2
+    ok=eunit:test({generator, fun decode_encode_varint_test_/0}).
 
-decode_encode_varstr() ->  % {{{1
+decode_encode_varstr() ->  % {{{2
     [].
-decode_encode_varstr(_Config) ->  % {{{1
-    eunit:test({generator, fun decode_encode_varstr_test_/0}).
+decode_encode_varstr(_Config) ->  % {{{2
+    ok=eunit:test({generator, fun decode_encode_varstr_test_/0}).
 
-encode_list() ->  % {{{1
+encode_list() ->  % {{{2
     [].
-encode_list(_Config) ->  % {{{1
-    eunit:test({generator, fun encode_list_test_/0}).
+encode_list(_Config) ->  % {{{2
+    ok=eunit:test({generator, fun encode_list_test_/0}).
 
-encode_network() ->  % {{{1
+encode_network() ->  % {{{2
     [].
-encode_network(_Config) ->  % {{{1
-    eunit:test({generator, fun encode_network_test_/0}).
+encode_network(_Config) ->  % {{{2
+    ok=eunit:test({generator, fun encode_network_test_/0}).
 
-encode_varint() ->  % {{{1
+encode_varint() ->  % {{{2
     [].
 
-encode_varint(_Config) ->  % {{{1
-    eunit:test({generator, fun encode_varint_test_/0}).
+encode_varint(_Config) ->  % {{{2
+    ok=eunit:test({generator, fun encode_varint_test_/0}).
 
-encode_varstr() ->  % {{{1
+encode_varstr() ->  % {{{2
     [].
-encode_varstr(_Config) ->  % {{{1
-    eunit:test({generator, fun encode_varstr_test_/0}).
+
+encode_varstr(_Config) ->  % {{{2
+    ok=eunit:test({generator, fun encode_varstr_test_/0}).
+
+pow() ->  % {{{2
+    [].
+
+pow(_Config) ->  % {{{2
+    ok=eunit:test({generator, fun pow_test_/0}).
+
+sqrt() ->  % {{{2
+    [].
+
+sqrt(_Config) ->  % {{{2
+    ok=eunit:test({generator, fun sqrt_test_/0}).
 
 %%%
-%%% EUnit test generators
+%%% EUnit test generators  {{{1
 %%%
 
-encode_varint_test_() ->  % {{{1
+
+encode_varint_test_() ->  % {{{2
     [
         ?_assert(bm_types:encode_varint(10) == <<10>>),
         ?_assert(bm_types:encode_varint(253) == <<16#fd, 00, 253>>),
@@ -150,7 +170,7 @@ encode_varint_test_() ->  % {{{1
         ?_assert(bm_types:encode_varint(4294967295) == <<16#fe, 16#ff, 16#ff, 16#ff, 16#ff>>),
         ?_assert(bm_types:encode_varint(4294967296) == <<16#ff, 00, 00, 00, 01, 00, 00, 00, 00>>)
         ].
-decode_encode_varint_test_() ->  % {{{1
+decode_encode_varint_test_() ->  % {{{2
     [
         ?_assert(bm_types:decode_varint(bm_types:encode_varint(10))== {10, <<>>}),
         ?_assert(bm_types:decode_varint(bm_types:encode_varint(253))== {253, <<>>}),
@@ -162,36 +182,36 @@ decode_encode_varint_test_() ->  % {{{1
         ?_assert(bm_types:decode_varint(<<(bm_types:encode_varint(4294967296))/bits, <<"test">>/bits>>)== {4294967296, <<"test">>})
         ].
 
-encode_varstr_test_() ->  % {{{1
+encode_varstr_test_() ->  % {{{2
     [
         ?_assert(bm_types:encode_varstr("1234567890") == <<10,"1234567890">>)
         ].
-decode_encode_varstr_test_() ->  % {{{1  % {{{1
+decode_encode_varstr_test_() ->  % {{{1  % {{{2
     [
         ?_assert(bm_types:decode_varstr(bm_types:encode_varstr("TEST")) == {"TEST", <<>>})
                 ].
 
-encode_list_test_() ->  % {{{1
+encode_list_test_() ->  % {{{2
     [
         ?_assert(bm_types:encode_list([1,2,3,4,5,6,7,8,9,0], fun bm_types:encode_varint/1) == <<10, 1,2,3,4,5,6,7,8,9,0>>),
         ?_assert(bm_types:encode_list([1,255,3,4,5,65536,7,8,9,0], fun bm_types:encode_varint/1) == <<10, 1,(bm_types:encode_varint(255))/bytes,3,4,5,(bm_types:encode_varint(65536))/bytes,7,8,9,0>>),
         ?_assert(bm_types:encode_list(["a", "b"], fun([O]) -> <<O>> end) == <<2, "a", "b">>)
                 ].
 
-decode_encode_list_test_() ->  % {{{1
+decode_encode_list_test_() ->  % {{{2
     [
         ?_assert(bm_types:decode_list(bm_types:encode_list([1,2,3,4,5,6,7,8,9,0], fun bm_types:encode_varint/1), fun bm_types:decode_varint/1) == {[1,2,3,4,5,6,7,8,9,0], <<>>}),
         ?_assert(bm_types:decode_list(bm_types:encode_list([1,255,3,4,5,65536,7,8,9,0], fun bm_types:encode_varint/1), fun bm_types:decode_varint/1) == {[1,255,3,4,5,65536,7,8,9,0], <<>>}),
         ?_assert(bm_types:decode_list(bm_types:encode_list(["a", "b"], fun([O]) -> <<O>> end), fun(<<O:8/integer, R/bytes>>)-> {[O], R}  end) == {["a", "b"], <<>>})
                 ].
 
-encode_network_test_() ->  % {{{1
+encode_network_test_() ->  % {{{2
     {ok, IP} = inet:parse_ipv4_address("127.0.0.1"),
     [
         ?_assert(bm_types:encode_network(#network_address{time=333, stream=1, ip=IP, port=8080, services=1} ) == <<333:64/big-integer, 1:32/big-integer, 1:64/big-integer, 0,0,0,0,0,0,0,0,0,0,255,255, 127,0,0,1, 8080:16/big-integer>>)
                 ].
 
-decode_encode_network_test_() ->  % {{{1
+decode_encode_network_test_() ->  % {{{2
     {ok, IP} = inet:parse_ipv4_address("127.0.0.1"),
     [
         ?_assert(bm_types:decode_network(bm_types:encode_network(#network_address{time=333,
@@ -206,3 +226,23 @@ decode_encode_network_test_() ->  % {{{1
                                       services=1},
                      <<>>})
                 ].
+
+pow_test_() ->  % {{{2
+    [
+        ?_assertEqual(bm_types:pow(2, 2), 4),
+        ?_assertEqual(bm_types:pow(3, 2), 9),
+        ?_assertEqual(bm_types:pow(4, 2), 16),
+        ?_assertEqual(bm_types:pow(5, 2), 25),
+        ?_assertEqual(bm_types:pow(2, 3), 8),
+        ?_assertEqual(bm_types:pow(2, 4), 16),
+        ?_assertEqual(bm_types:pow(2, 5), 32)
+    ].
+
+sqrt_test_() ->  % {{{2
+    [
+        ?_assertEqual(bm_types:sqrt(1), 1),
+        ?_assertEqual(bm_types:sqrt(4), 2),
+        ?_assertEqual(bm_types:sqrt(9), 3),
+        ?_assertEqual(bm_types:sqrt(16), 4),
+        ?_assertEqual(bm_types:sqrt(25), 5)
+    ].
